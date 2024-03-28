673150fcca17f3d92167571d5f6d7a4061edb658
Simi <flosimi57@gmail.com>
2024-03-28 11:25:05 +0200

    a

---
+++ main.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+
+def convert_to_mp3(input_file,output_file):
+    ffmpeg_cmd=[
+        "ffmpeg",
+        "-i",input_file,
+        "-vn",
+        "-acodec","libmp3lame",
+        "-ab","192k",
+        "-ar","44100",
+        "-y",
+        output_file
+    ]
+    try:
+        subprocess.run(ffmpeg_cmd,check=True)
+    except subprocess.CalledProcessError as e:
+        print("Conversion failed! ")
+
+convert_to_mp3("Moves Like Jagger.mp4","audio.mp3")
---
+++ main_2.py
@@ -0,0 +1,18 @@
+import moviepy.editor
+from tkinter.filedialog import askopenfilename
+import tkinter as tk
+
+def convert_to_mp3():
+    video_file = askopenfilename()
+    video=moviepy.editor.VideoFileClip(video_file)
+    audio = video.audio
+    audio.write_audiofile("sample.mp3")
+    print("Conversion Completed")
+root = tk.Tk()
+root.title("MP3 Converter")
+root.geometry("900x260")
+convert_button = tk.Button(root,text="Convert to mp3",command=convert_to_mp3)
+
+convert_button.pack()
+
+root.mainloop()
